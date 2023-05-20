8e95b4697de560662e4ec693dd8102ec17c8333e
BharateshPoojary <bharateshpoojari@gmail.com>
2023-05-20 05:43:45 +0530

    addedscrllbar

--- Project 1/index.js
+++ Project 1/index.js
@@ -1,25 +1,21 @@
 const apikey ="5914067868644595e99f1fce7a4ffa3d";
+const halfImagePath="https://image.tmdb.org/t/p/w500"
 const movieAccordingTognreId="https://api.themoviedb.org/3/discover/movie?api_key="+apikey+"&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&with_genres="+28;
 const gnreIdListApi="https://api.themoviedb.org/3/genre/movie/list?api_key="+apikey+"&language=en-US"
 const popularmovies = "https://api.themoviedb.org/3/movie/popular?api_key="+apikey+"&language=en-US&page=1";
 const latestmovies = "https://api.themoviedb.org/3/movie/latest?api_key="+apikey+"&language=en-US"
 window.addEventListener("load",async ()=>{
-
-    let pm=await (await fetch(popularmovies)).json();
-    console.log(pm);
-
-    let gnrePr=await (await fetch(movieAccordingTognreId)).json();
-    console.log(gnrePr);
-    let lm=await(await fetch(latestmovies)).json();
-    console.log(lm);
-    let halfImagePath="https://image.tmdb.org/t/p/w500"
-    let posterPath=gnrePr.results[1].poster_path;
-    let fullImagePath=halfImagePath+posterPath;
-    console.log(fullImagePath);
-})
-let imgTag=document.createElement("img");
-imgTag.src="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg";
-imgTag.height="220"
-imgTag.width="150"
-let posterContainer=document.querySelector(".poster-container");
-posterContainer.appendChild(imgTag);
+         for( i = 0;i<=19;i++){
+              let posterPath = await(await(await (await fetch(movieAccordingTognreId)).json())).results[i].poster_path;     
+              let fullposterpath=halfImagePath+posterPath;     
+              let poster=document.createElement("img");
+              poster.src=fullposterpath;
+              poster.height="170";
+              poster.width="120";
+              poster.style.paddingRight="2px";
+              let appendposter=document.querySelector(".poster-container");
+              appendposter.appendChild(poster);    
+        }   
+       
+    });  
+    
