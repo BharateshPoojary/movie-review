1a9a1720c28ab78528afc812dbf7af1592e6681f
BharateshPoojary <bharateshpoojari@gmail.com>
2023-05-21 07:56:10 +0530

    genreid found

--- Project 1/index.js
+++ Project 1/index.js
@@ -1,12 +1,13 @@
 const apikey ="5914067868644595e99f1fce7a4ffa3d";
 const halfImagePath="https://image.tmdb.org/t/p/w500"
-const movieAccordingTognreId="https://api.themoviedb.org/3/discover/movie?api_key="+apikey+"&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&with_genres="+28;
 const gnreIdListApi="https://api.themoviedb.org/3/genre/movie/list?api_key="+apikey+"&language=en-US"
 const popularmovies = "https://api.themoviedb.org/3/movie/popular?api_key="+apikey+"&language=en-US&page=1";
 const latestmovies = "https://api.themoviedb.org/3/movie/latest?api_key="+apikey+"&language=en-US"
+const movieAccordingTognreId="https://api.themoviedb.org/3/discover/movie?api_key="+apikey+"&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&with_genres=";
+                
 window.addEventListener("load",async ()=>{
          for( i = 0;i<=19;i++){
-              let posterPath = await(await(await (await fetch(movieAccordingTognreId)).json())).results[i].poster_path;     
+              let posterPath = await(await(await (await fetch(popularmovies)).json())).results[i].poster_path;     
               let fullposterpath=halfImagePath+posterPath;     
               let poster=document.createElement("img");
               poster.src=fullposterpath;
@@ -15,8 +16,28 @@ window.addEventListener("load",async ()=>{
               poster.style.paddingRight="2px";
               let appendposter=document.querySelector(".poster-container");
               appendposter.appendChild(poster);    
-        }   
-       
-    });  
+        }
+        
+        genreidlist = await (await (await fetch(gnreIdListApi)).json()).genres;
+        genreidlist.forEach(genre=> {
+                allgenremovies(genre)
+        }) 
+        function allgenremovies(genre) {
+            genreidforeachmovie=movieAccordingTognreId+genre.id;
+            console.log(genreidforeachmovie); 
+            sectionnameforeachmovie=movieAccordingTognreId+genre.name;
+            console.log(sectionnameforeachmovie);  
+        }
+        results=await(await(await fetch(genreidforeachmovie)).json()).results;
+         genreidforeachmovie.forEach(results=> {
+          
+            console.log(results);  
+        });
     
+      
+       
+       
+        });
+    
+        
 
